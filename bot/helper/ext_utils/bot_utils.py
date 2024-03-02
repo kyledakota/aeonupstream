83f0646087009bd92c568d7354d3d31aa9cbe541
kyledakota <161427517+kyledakota@users.noreply.github.com>
2024-03-02 21:46:01 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -170,11 +170,11 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"<b>`{escape(f'{download.name()}')}`</b>\n\n"
+        msg += f"<b>{escape(f'{download.name()}')}</b>\n\n"
         msg += f"<b>{download.status()}...</b>\n"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<code>{progress_bar(download.progress())}</code>  {download.progress()}\n"
-            msg += f"\n╒═════"
+            msg += f"\n╒═════\n"
             msg += f"\n Progress: {download.processed_bytes()} of {download.size()}"
             msg += f"\n User: {source(download)}"
             msg += f"\n Speed: {download.speed()}"
@@ -194,8 +194,8 @@ def get_readable_message():
         else:
             msg += f"\n Size: {download.size()}"
         msg += f"\n Elapsed: {get_readable_time(time() - download.message.date.timestamp())}\n"
-        msg += f"\n✋🏻/stop_{download.gid()[:8]}\n\n"
-        msg += f"\n╘══════"
+        msg += f"\n✋🏻/stop_{download.gid()[:8]}\n"
+        msg += f"╘══════"\n\n
     if len(msg) == 0:
         return None, None
     dl_speed = 0
