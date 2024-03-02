b19d06110a9dd8a6692d9a52145f01c1418ad119
kyledakota <161427517+kyledakota@users.noreply.github.com>
2024-03-02 22:08:50 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -193,9 +193,9 @@ def get_readable_message():
             msg += f"\n Time: {download.seeding_time()}"
         else:
             msg += f"\n Size: {download.size()}"
-        msg += f"\n Elapsed: {get_readable_time(time() - download.message.date.timestamp())}\n"
-        msg += f"\n✋🏻/stop_{download.gid()[:8]}\n\n"
+        msg += f"\n Elapsed: {get_readable_time(time() - download.message.date.timestamp())}\n\n"
         msg += f"╘════════════════\n\n"
+        msg += f"\n✋🏻/stop_{download.gid()[:8]}\n\n"        
     if len(msg) == 0:
         return None, None
     dl_speed = 0
