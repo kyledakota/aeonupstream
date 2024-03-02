a53a9094d5fb87f9f7a6ecc7045693bac477c11b
kyledakota <161427517+kyledakota@users.noreply.github.com>
2024-03-02 22:00:41 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -170,16 +170,16 @@ def get_readable_message():
         globals()['STATUS_START'] = STATUS_LIMIT * (PAGES - 1)
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
-        msg += f"<b>☘️<ul>{escape(f'{download.name()}')}</ul></b>\n\n"
+        msg += f"<b>☘️ {escape(f'{download.name()}')}</b>\n\n"
         msg += f"<b>{download.status()}...</b>\n"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<code>{progress_bar(download.progress())}</code>  {download.progress()}\n"
             msg += f"\n╒═════\n"
-            msg += f"\n Progress: {download.processed_bytes()} of {download.size()}"
-            msg += f"\n User: {source(download)}"
-            msg += f"\n Speed: {download.speed()}"
-            #!/ msg += f"\n Engine: {download.engine}"
-            msg += f'\n Estimated: {download.eta()}'            
+            msg += f"\n 🟢 Progress: {download.processed_bytes()} of {download.size()}"
+            msg += f"\n 📎 User: {source(download)}"
+            msg += f"\n ᯤ Speed: {download.speed()}"
+            #!/ msg += f"\n ⚙ Engine: {download.engine}"
+            msg += f'\n 🏋 Estimated: {download.eta()}'            
             if hasattr(download, 'seeders_num'):
                 try:
                     msg += f"\n Seeders: {download.seeders_num()} | Leechers: {download.leechers_num()}"
